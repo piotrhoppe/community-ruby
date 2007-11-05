0eb7a5c9a319b8bc4eea4c16436f2ce5e4adb04f
tor <tor@netbeans.org>
2007-11-05 03:55:45 +0000

    Fix surround with template filtering; recently the surround with hint seems to also enable templates without contexts, so fix that. Also port Retouche fixes around open editor/source task management (which uses the new editor library rather than Registry hacks), and tweak the hints infrastructure to support extra hints around selection.

--- ruby.hints/src/org/netbeans/modules/ruby/hints/spi/SelectionRule.java
+++ ruby.hints/src/org/netbeans/modules/ruby/hints/spi/SelectionRule.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,64 +20,36 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
  * Contributor(s):
- *
+ * 
  * Portions Copyrighted 2007 Sun Microsystems, Inc.
  */
+
 package org.netbeans.modules.ruby.hints.spi;
 
 import java.util.List;
-import org.netbeans.api.gsf.OffsetRange;
-import org.openide.filesystems.FileObject;
+import java.util.Map;
+import org.netbeans.api.gsf.CompilationInfo;
 
 /**
- * Wrapper around org.netbeans.spi.editor.hints.ErrorDescription
+ * Represents a rule to be run on text selection
  *
  * @author Tor Norbye
  */
-public class Description {
-    private final String description;
-    private final List<Fix> fixes;
-    private final FileObject file;
-    private final OffsetRange range;
-    private final Rule rule;
-    private int priority;
-    
-    public Description(Rule rule, String description, FileObject file, OffsetRange range, List<Fix> fixes, int priority) {
-        this.rule = rule;
-        this.description = description;
-        this.file = file;
-        this.range = range;
-        this.fixes = fixes;
-        this.priority = priority;
-    }
-    
-    public Rule getRule() {
-        return this.rule;
-    }
+public interface SelectionRule extends Rule {
 
-    public String getDescription() {
-        return description;
-    }
-
-    public FileObject getFile() {
-        return file;
-    }
-
-    public List<Fix> getFixes() {
-        return fixes;
-    }
-
-    public OffsetRange getRange() {
-        return range;
-    }
-    
-    public int getPriority() {
-        return priority;
-    }
-    
-    public String toString() {
-        return "Description(desc=" + description + ",fixes=" + fixes + ")";
-    }
+    public void run(CompilationInfo compilationInfo, int start, int end,
+             List<Description> result, Map<String,Object> context);
 }
